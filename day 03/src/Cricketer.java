cf28c37686b59694e38523a251c10ce1320d0ad4
sunan <sunan@192.168.1.4>
2022-06-02 00:13:02 +0530

    first commit

---
+++ day 03/src/Cricketer.java
@@ -0,0 +1,11 @@
+
+ class Cricketer {
+    String name = "Dhoni";
+    int age = 39;
+    String color ="Brown";
+    String role="Batsaman";
+    String nationality = "Indian";
+    void Display() {
+    	System.out.println("I LOVE Criket");
+    }
+}
---
+++ day 03/src/Employee.java
@@ -0,0 +1,11 @@
+
+public class Employee {
+   public static void main (String[] args) {
+	   
+	   Employee e1 = new Employee();
+	   Employee e2 = new Employee();
+	   
+	   e1.display();
+	   e2.display();
+   }
+}
