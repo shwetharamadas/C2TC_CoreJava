eb7e8859fa0ba12189a2a6925bd658fedaa91f8d
sunan <sunan@LAPTOP-ISPM6TLU>
2022-06-16 15:37:50 +0530

    fifth commit

---
+++ Set/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main { 
+public static  void main(String args[])
+{
+	Set obj=new HashSet();
+	obj.add("Technology");
+	obj.add("Practice");
+	obj.add("Java");
+	obj.add(4.56);
+	obj.add(1);
+	System.out.println(obj);
+}
+
+}
